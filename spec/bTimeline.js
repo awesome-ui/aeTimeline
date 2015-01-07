1372e1313896d885eff2ced5e41637e806120d6f
Michael F <tehfreak@tehfreak.com>
2015-01-07 22:25:35 +0200

    тесты

--- spec/bTimeline.js
+++ spec/bTimeline.js
@@ -51,6 +51,43 @@ describe('bTimeline', function() {
             assert.isDefined(elementScope.intervals)
             assert.strictEqual(elementScope.intervals, intervals)
         }))
+
+    })
+
+    describe('bTimeline controller', function () {
+
+        var scope, element, elementScope
+
+        beforeEach(inject(function ($compile, $rootScope) {
+            scope= $rootScope.$new()
+            scope.getBeginDate= function () {
+                return beginDate
+            }
+            scope.getEndDate= function () {
+                return endDate
+            }
+            scope.getIntervals= function () {
+                return intervals
+            }
+            element= angular.element('<div b-timeline timeline-begin-date="getBeginDate()" timeline-end-date="getEndDate()" timeline-intervals="getIntervals()"></div>')
+            $compile(element)(scope)
+            scope.$digest()
+            elementScope= element.isolateScope()
+        }))
+
+        it('exists', inject(function () {
+            assert.isDefined(elementScope)
+            assert.isDefined(elementScope.bTimeline)
+        }))
+
+        it('expose #calcIntervalLeft', inject(function () {
+            assert.isDefined(elementScope.bTimeline.calcIntervalLeft)
+        }))
+
+        it('expose #calcIntervalRight', inject(function () {
+            assert.isDefined(elementScope.bTimeline.calcIntervalRight)
+        }))
+
     })
 
 })
